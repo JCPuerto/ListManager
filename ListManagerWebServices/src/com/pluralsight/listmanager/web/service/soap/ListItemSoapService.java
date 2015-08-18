1d9fd8c62fea90c5f51640d8ae9312d6384c5de5
Juan Carlos Puerto <jc@jcpuerto.com>
2015-08-17 20:50:45 -0400

    Unused usings removed

--- ListManagerWebServices/src/com/pluralsight/listmanager/web/service/soap/ListItemSoapService.java
+++ ListManagerWebServices/src/com/pluralsight/listmanager/web/service/soap/ListItemSoapService.java
@@ -5,30 +5,30 @@ import javax.jws.WebParam;
 import javax.jws.WebService;
 
 import com.pluralsight.listmanager.model.ListItem;
-import com.pluralsight.listmanager.model.User;
 import com.pluralsight.listmanager.service.ListItemService;
 import com.pluralsight.listmanager.service.impl.ListItemServiceImpl;
 
 @WebService(serviceName = "ListItemSoapService")
 public class ListItemSoapService {
 	private final ListItemService listItemService;
-	
+
 	public ListItemSoapService() {
 		listItemService = new ListItemServiceImpl();
 	}
-	
+
 	@WebMethod
-	public ListItem add(@WebParam(name = "userId") Long userId, @WebParam(name = "value") String value){
+	public ListItem add(@WebParam(name = "userId") Long userId, @WebParam(name = "value") String value) {
 		return listItemService.addListItem(userId, value);
 	}
-	
+
 	@WebMethod
-	public ListItem update(@WebParam(name = "userId") Long userId, @WebParam(name = "listItemId") Long listItemId, @WebParam(name = "value") String value){
+	public ListItem update(@WebParam(name = "userId") Long userId, @WebParam(name = "listItemId") Long listItemId,
+			@WebParam(name = "value") String value) {
 		return listItemService.updateListItem(userId, listItemId, value);
 	}
-	
+
 	@WebMethod
-	public void delete(@WebParam(name = "userId") Long userId, @WebParam(name = "listItemId") Long listItemId){
+	public void delete(@WebParam(name = "userId") Long userId, @WebParam(name = "listItemId") Long listItemId) {
 		listItemService.deleteListItem(userId, listItemId);
 	}
 }
